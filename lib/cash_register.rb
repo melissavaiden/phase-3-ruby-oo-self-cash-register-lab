f91ab4750aadd068a33f8a563d0aabc7310546d7
Melissa Vaiden <melissavaiden@gmail.com>
2022-09-08 18:19:10 -0400

    Completed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister
+    attr_accessor :discount
+    attr_accessor :total
+    attr_accessor :items
+    attr_accessor :last_item_price
 
+    def initialize discount = 0
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item (title, price, quantity = 1)
+        self.total += price * quantity
+        @last_item_price = price * quantity
+        quantity.times {self.items << title}
+    end
+
+    def apply_discount
+        if @discount > 0
+            self.total -= (self.total * @discount / 100)
+            "After the discount, the total comes to $#{self.total}."
+        else 
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        self.total -= @last_item_price
+    end
+end
